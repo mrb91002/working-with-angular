c5bcf2354fb0a949d298784352b32dd1120da1b7
Matthew Benavides <matthewrbenavides@gmail.com>
2016-11-14 20:14:47 -0800

    Working on learning directives

--- public/app.js
+++ public/app.js
@@ -7,24 +7,78 @@ myApp.config(function($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'pages/main.html',
-      controller: 'mainController'
+      controller: 'mainController',
     })
 
     .when('/second', {
       templateUrl: 'pages/second.html',
-      controler: 'secondController'
+      controller: 'secondController'
     })
+
+    .when('/third/', {
+      templateUrl: 'pages/third.html',
+      controller: 'thirdController'
+    })
+
+    .when('/third/:num', {
+      templateUrl: 'pages/third.html',
+      controller: 'thirdController'
+    })
+
 });
 
-// Controller
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+myApp.service('nameService', function() {
 
-  // $scope.name = 'Matt';
+  const self = this;
+  this.name = 'nameService.name!!!';
+  this.name2 = '123141234123412431414312412412';
+  this.car = 'jeep wrangler';
+
+
+  this.nameLength = function() {
+    return self.name.length;
+  }
+});
+
+myApp.directive('directiveOne', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/test-directive.html',
+    replace: true,
+    scope: {
+      name: '@',
+      nameService: '=',
+      testFunction: '&'
+    }
+  }
+})
+
+// Controller
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+
+  $scope.name = 'Matt';
+  $log.log(nameService.name, nameService.nameLength(), nameService.nameLength);
+  $scope.nameService = nameService;
+
 
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+myApp.controller('secondController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
 
-  // $scope.name = 'Ryan';
+  $scope.name = 'Ryan';
+  $scope.nameService = nameService;
+  $scope.testFunction = function(name, string) {
+    return 'Result of function:' + name + string;
+  }
 
+}]);
+
+myApp.controller('thirdController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
+
+  $scope.name = 'Third page name slot';
+  $scope.something = 'something';
+  $scope.num = $routeParams.num || "default";
+  $log.info(nameService)
+
+  $scope.nameService = nameService;
 }]);
